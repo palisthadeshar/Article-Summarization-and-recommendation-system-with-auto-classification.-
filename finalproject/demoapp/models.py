2fbd8290b0c73c9d38ff55a503bc0ef56931fb5e
palisthadeshar <palisthadeshar@gmail.com>
2023-04-28 09:20:00 +0545

    project

---
+++ finalproject/demoapp/models.py
@@ -0,0 +1,7 @@
+from djongo import models
+
+# Create your models here.
+class USERS(models.Model):
+    name = models.CharField(max_length=100)
+    #email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
---
+++ finalproject/demoapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def HomePage(request):
+    return render (request,'home.html')
+
+def SignupPage(request):
+    
+    return render (request,'register.html')
+
+def LoginPage(request):
+    
+    return render (request,'login.html')
